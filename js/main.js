cbfd1260ae5fe6c42066d7bf7756111661e092a8
Mike Schoenrock <hello@michaelschoenrock.com>
2014-03-25 00:22:48 +0100

    Merge branch 'master' of https://github.com/flymke/PG_Test
    Conflicts:
    	index.html
    	js/main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 var app = {
+    
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+<<<<<<< HEAD
+    
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+=======
+>>>>>>> FETCH_HEAD
 
-    findByName: function() {
+    /*findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
@@ -11,10 +27,27 @@ var app = {
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
         });
+    },*/
+    
+    findByName: function() {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
     },
 
     initialize: function() {
-        this.store = new WebSqlStore();
+        var self = this;
+<<<<<<< HEAD
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.store = new WebSqlStore(function() {
+            self.renderHomeView();
+=======
+        this.store = new WebSqlStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+>>>>>>> FETCH_HEAD
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
